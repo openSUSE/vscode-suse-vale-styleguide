9d6debddaf5e4dc68a4bee7203be3412b70bf2ba
Tomáš Bažant <tbazant@suse.cz>
2023-06-21 17:28:56 +0200

    Regenrating .vale.ini on extension startup

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require('fs');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,22 +10,35 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "suse-vale-styleguide" is now active!');
-
-    // get config obj for vale plugin
-    let valeConfiguration = vscode.workspace.getConfiguration("vale");
-    // get the current .vale.ini path
-    let currentValeIniPath = valeConfiguration.get("valeCLI.config");
-    // get info about old vale.ini
-    vscode.window.showInformationMessage(currentValeIniPath);
-
-    let suseValeConfigPath = context.extensionPath;
-    valeConfiguration.update("valeCLI.config", suseValeConfigPath + "/.vale.ini");
+    // get config obj for Vale plugin and construct path to .vale.ini
+    let valeConfig = vscode.workspace.getConfiguration('vale');
+    let curValeConfigPath = context.extensionPath + '/.vale.ini';
+    console.log('Extension path to .vale.ini :' + curValeConfigPath);
+    // get global storage URI and create if needed
+    let globalStorageURI = context.globalStorageUri;
+    console.log('Global storage: ' + globalStorageURI.path);
+    if (fs.existsSync(globalStorageURI.path) == false) {
+        fs.mkdir(globalStorageURI.path);
+    }
+    let globalValeConfigPath = globalStorageURI.path + '/.vale.ini';
+    console.log('Global path to .vale.ini :' + globalValeConfigPath);
+    //compile the content of custom .vale.ini
+    let valeIni = 'StylesPath = ' + context.extensionPath + '\n' +
+        'MinAlertLevel = suggestion\n' +
+        '[asciidoctor]\n' +
+        'experimental = YES\n' +
+        '[*.xml]\n' +
+        'Transform = ' + context.extensionPath + '/xslt/vale-docbook.xsl\n' +
+        'BasedOnStyles = styles\n' +
+        '[*.html]\n' +
+        'BasedOnStyles = styles\n' +
+        '[*.{ md, txt, adoc }]\n' +
+        'BasedOnStyles = styles';
+    console.log(valeIni);
+    //write the content of .vale.ini into the global strage file
+    fs.writeFileSync(globalValeConfigPath, valeIni);
+    valeConfig.update('valeCLI.config', globalValeConfigPath);
 }
-
 // This method is called when your extension is deactivated
 function deactivate() { }
 
